27a57d147f5699149096e5039d8214017f54b8e6
PAWAN KUMAR YADAV <163421764+pawanyadav0104@users.noreply.github.com>
2025-05-25 15:47:10 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chatapplication-1-io07.onrender.com" : "/api",
   withCredentials: true,
 });
